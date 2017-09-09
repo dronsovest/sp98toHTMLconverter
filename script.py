ae72827e137b0f8df0f0c097900aceef7c8b2828
dronsovest <3124498@mail.ru>
2017-09-09 12:41:24 +0300

    Add files via upload

--- script.py
+++ script.py
@@ -1,5 +1,10 @@
-htmlData = "<table width=" + "450" + "><caption>Рукодельный швейцарский классический турнир</caption>"
-oldFile = open("Test Trump.txt")
+import header
+import players
+
+information = input("Введите названите турнира, номер тура или другую информацию для отображения в загаловке таблицы: ")
+
+htmlData = "<table width=" + "450" + "><caption>" + information + "</caption>"
+oldFile = open("Data.txt", encoding="cp1251")
 
 
 for line in oldFile:
@@ -7,13 +12,17 @@ for line in oldFile:
 	for item in line:
 		if item == "|":
 			htmlData = htmlData + "</td><td>" 
-		elif item == '\n':
+		elif item == "\n":
 			continue
 		else:
 			htmlData = htmlData + item
 	htmlData = htmlData + "</td></tr>"
 
 htmlData = htmlData + "</table>"
+for key in players.players:
+	htmlData = htmlData.replace(key, players.players[key])
+for key in header.headers:
+	htmlData = htmlData.replace(key, header.headers[key])
 f = open('result.txt', 'w')
 f.write(htmlData);
 f.close()
